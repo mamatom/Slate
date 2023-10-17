a45a3dee9e50814cdca0e2600dc3a8235eef0153
mamatom <icezamamatom@gmail.com>
2023-10-17 18:39:35 +0700

    updated metadata and workflow

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -2,16 +2,15 @@
 export default {
 	meta: {
 		name: 'Slate',
-		author: 'Gibbu & Tropical',
-		description: `An optimized, consistent, and functional theme for Discord based on GitHub's design language.`,
+		author: 'mamatom',
+		description: `A fork of DiscordStyles/Slate`,
 		version: '1.2',
-		source: 'https://github.com/DiscordStyles/Slate/',
-		invite: 'TeRQEPb'
+		source: 'https://github.com/mamatom/Slate/',
 	},
 	base: {
 		output: 'dist/dist'
 	},
-	baseImport: 'https://discordstyles.github.io/Slate/dist/Slate.css',
+	baseImport: 'https://mamatom.github.io/Slate/dist/Slate.css',
 	addons: [
 		['./src/addons/_toolbar-actions.scss', 'dist/ToolbarActions.css'],
 		['./src/addons/_serverlist.scss', 'dist/Serverlist.css']
